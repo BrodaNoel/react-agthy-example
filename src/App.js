ec87eb4a4cd318c45b40cc92e7f8cf07a54cbaf0
Broda Noel <noel@olapic.com>
2017-06-06 00:34:26 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to the example page of `react-agthy` component</h2>
+        </div>
+        <p className="App-intro">
+          To use the component, click on Agthy component, in the right-bottom corner.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
